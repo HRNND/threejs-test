574eb5ff8a6443c506f4bddb4fe948627e83649d
HRNND <79288781+HRNND@users.noreply.github.com>
2026-05-11 10:17:13 +0700

    import 3d model

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ controls.screenSpacePanning = true; // Allows vertical/horizontal panning
 // --- 4. IMPORTING THE 3D MODEL ---
 // Replace 'public/model.glb' with your actual file path.
 const loader = new GLTFLoader();
-loader.load('/model.glb', function (gltf) {
+loader.load('/idcard.glb', function (gltf) {
     const model = gltf.scene;
     scene.add(model);
     
