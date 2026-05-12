d2731533b53101470656b51ba93330fc2df49a12
HRNND <79288781+HRNND@users.noreply.github.com>
2026-05-12 13:47:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ controls.enableDamping = true;
 const loader = new GLTFLoader();
 
 // Use the filename exactly as it appears in GitHub
-loader.load('bot_follow_cursor_a-6.glb', function (gltf) {
+loader.load('bot_follow_cursor_a-8.glb', function (gltf) {
     const model = gltf.scene;
     scene.add(model);
 
