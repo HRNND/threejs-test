6a32b549759c90f9658bf11c5efb0a73b8fa478f
HRNND <79288781+HRNND@users.noreply.github.com>
2026-05-12 13:37:33 +0700

    Update GLB file name for model loading

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ controls.enableDamping = true;
 const loader = new GLTFLoader();
 
 // Use the filename exactly as it appears in GitHub
-loader.load('bot_follow_cursor_a-4.glb', function (gltf) {
+loader.load('bot_follow_cursor_a-5.glb', function (gltf) {
     const model = gltf.scene;
     scene.add(model);
 
